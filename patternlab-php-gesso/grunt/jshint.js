dec79409cc1cc99fe8b114a4a6af1121cf486c45
Laura Davis-Robeson <ldavisrobeson@forumone.com>
2015-04-09 15:30:16 -0700

    Remove Pattern Lab-specific Grunt tasks I'm not actively using

--- patternlab-php-gesso/grunt/jshint.js
+++
@@ -1,6 +0,0 @@
-// JSHint: validate JavaScript
-// https://github.com/gruntjs/grunt-contrib-jshint
-
-// ADD TASK
-// module.exports = {
-// };
--- patternlab-php-gesso/grunt/newer.js
+++
@@ -1,7 +0,0 @@
-// Newer: configure another task to run with src files that are
-// either newer than the dest files or newer than the last
-// successful run
-// https://github.com/tschaub/grunt-newer
-
-// This task doesn't require any special configuration.
-// To use it, just add newer as the first argument when running other tasks, e.g., 'newer:csslint'.
